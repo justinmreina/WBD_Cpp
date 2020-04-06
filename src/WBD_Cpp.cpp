c4fb1647378e82e81ccfe3a11b5d033cb2e2c170
Justin Reina <Justin Reina>
2020-04-05 22:02:03 -0700

    (+)    initial commit

---
+++ src/WBD_Cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : WBD_Cpp.cpp
+// Author      : Jmr
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
